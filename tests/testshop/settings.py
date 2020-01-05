b5fdfc935ab516a613fa58a84ec07f7e22cbf07f
Nicolas PASCAL <np.pascal@gmail.com>
2020-01-06 00:11:44 +0100

    Update settings.py

--- tests/testshop/settings.py
+++ tests/testshop/settings.py
@@ -13,7 +13,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': ':memory:',
-    }
+        'TEST': {
+            'NAME':  ':memory:',
+         },
+         },   
 }
 
 STATIC_URL = '/static/'
